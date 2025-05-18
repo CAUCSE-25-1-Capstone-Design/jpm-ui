822f9deaf679891387c14a445cba3c771eab7e6c
JaehwanH <jec0101@naver.com>
2025-05-18 20:27:42 +0900

    feat: JpmConstants에 정의된 DEVELOPMENT_LEVEL에 따라 jpm-nlp 경로 조절 및 로그 출력 레벨 조정

---
+++ src/main/java/jpm/ui/constants/JpmConstants.java
@@ -0,0 +1,19 @@
+package jpm.ui.constants;
+
+public class JpmConstants {
+
+    public static final DevelopmentLevel DEVELOPMENT_LEVEL = DevelopmentLevel.RELEASE;
+    private static final String DEBUG_JPM_NLP_PATH = "src/main/resources/python/gpt-toolCall.py";
+    private static final String RELEASE_JPM_NLP_PATH = "./gpt-toolCall.py";
+//    private static final String DEBUG_JPM_CORE_PATH = "";
+//    private static final String RELEASE_JPM_CORE_PATH = "";
+
+    public static final String JPM_NLP_PATH = DEVELOPMENT_LEVEL == DevelopmentLevel.DEBUG
+            ? DEBUG_JPM_NLP_PATH
+            : RELEASE_JPM_NLP_PATH;
+//
+//    public static final String JPM_CORE_PATH = DEVELOPMENT_LEVEL == DevelopmentLevel.DEBUG
+//            ? DEBUG_JPM_CORE_PATH
+//            : RELEASE_JPM_CORE_PATH;
+}
+
